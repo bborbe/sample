b0ac84186ed54efa9e41e01051cf1bb57bb97fa2
Benjamin Borbe <bborbe@rocketnews.de>
2013-04-19 15:09:18 +0200

    Add simple test

--- sample_guitest/src/main/java/selenium/Runner.java
+++ sample_guitest/src/main/java/selenium/Runner.java
@@ -1,5 +1,6 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
@@ -21,8 +22,28 @@ public class Runner {
 		WebDriver driver = null;
 		try {
 			driver = new FirefoxDriver();
+
+			// http://docs.seleniumhq.org/docs/04_webdriver_advanced.jsp
+			// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 			driver.get("http://www.heise.de");
+
+			logger.debug("title: " + driver.getTitle());
+			logger.debug("currentUrl: " + driver.getCurrentUrl());
+			logger.debug("pageSource.length: " + driver.getPageSource().length());
+			logger.debug("windowHandle: " + driver.getWindowHandle());
+			logger.debug("windowHandles: " + driver.getWindowHandles());
+
+			driver.findElement(By.xpath("//*[@id=\"themen_aktuell\"]/ol/li[4]/a")).click();
+
+			logger.debug("text: " + driver.findElement(By.xpath("//*[@id=\"mitte_uebersicht\"]/div[1]/h1")).getText());
+
+			logger.debug("title: " + driver.getTitle());
+			logger.debug("currentUrl: " + driver.getCurrentUrl());
+			logger.debug("pageSource.length: " + driver.getPageSource().length());
+			logger.debug("windowHandle: " + driver.getWindowHandle());
+			logger.debug("windowHandles: " + driver.getWindowHandles());
+
 		} catch (Exception e) {
 			logger.error("Exception", e);
 		} finally {
