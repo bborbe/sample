1bb65319867f0ebb7d100a0577f0b152b2dee178
Benjamin Borbe <bborbe@rocketnews.de>
2015-02-06 14:10:51 +0100

    add sprint security sample

---
+++ sample_spring_security/src/main/java/example/config/core/SpringMvcInitializer.java
@@ -0,0 +1,23 @@
+package example.config.core;
+
+import example.config.AppConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
---
+++ sample_spring_security/src/main/java/example/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package example.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
