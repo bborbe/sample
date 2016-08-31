70c8f049e17568bac68e8728d7e33815cc173c66
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-31 11:56:58 +0200

    add rx sample

---
+++ sample_rx/src/main/java/example/Test.java
@@ -0,0 +1,32 @@
+package example;
+
+import rx.Observable;
+import rx.Observer;
+
+public class Test {
+
+	public static void main(String[] args) {
+		getUsernames().subscribe(new Observer<String>() {
+
+			@Override
+			public void onNext(final String s) {
+				System.out.println("get: " + s);
+			}
+
+			@Override
+			public void onCompleted() {
+				System.out.println("completed");
+			}
+
+			@Override
+			public void onError(final Throwable e) {
+				System.err.println("error: " + e);
+			}
+
+		});
+	}
+
+	private static Observable<String> getUsernames() {
+		return null;
+	}
+}
